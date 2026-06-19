d13632cd51137c4ff4e7f3fcce79063321793922
3A <74498981+3agithub@users.noreply.github.com>
2026-06-19 15:44:11 +0800

    0.4.3 - alias lists for subcommands

--- cogs/alias.py
+++ cogs/alias.py
@@ -7,12 +7,12 @@ class Alias(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(aliases=["a", "aliases"])
+    @commands.group(invoke_without_command=True, aliases=["a", "aliases"])
     async def alias(self, ctx: commands.Context):
         embed=discord.Embed(
             title="NotPogMC Command Aliases",
-            description="The following is a list of all aliases for commands.", color=discord.Color(0xfff5d1),
-            
+            description="The following is a list of all aliases for commands.",
+            color=discord.Color(0xfff5d1),
             timestamp=datetime.datetime.now(datetime.timezone.utc)
         )
         embed.add_field(
@@ -41,8 +41,101 @@ class Alias(commands.Cog):
                     `m!credits` - `m!cre`",
             inline=True
         )
+        embed.add_field(
+            name="Tip:",
+            value=f"You can use `m!alias <command>` to view aliases for subcommands of a specific command.",
+            inline=False
+        )
         embed.set_author(name = ctx.author.name, icon_url = ctx.author.display_avatar.url)
-        embed.set_footer(text=f"Aliases are still in progress. / means the command does not have an alias.\nThis is not the place to look for help. If you're finding help, please check m!help for all commands and their uses. · Shard #{ctx.guild.shard_id + 1}")
+        embed.set_footer(text=f"Shard #{ctx.guild.shard_id + 1}")
+        await ctx.send(embed=embed)
+
+    @alias.command(aliases=["m"])
+    async def mine(self, ctx: commands.Context):
+        embed=discord.Embed(
+            title="NotPogMC Command Aliases · m!mine",
+            description="`m!mine` - `m!m`",
+            color=discord.Color(0xfff5d1),
+            timestamp=datetime.datetime.now(datetime.timezone.utc)
+        )
+        embed.add_field(
+            name="Subcommands",
+            value=f"`m!mine overworld` - `m!m o`, `m!m ow`\n\
+                    `m!mine nether` - `m!m n`\n\
+                    `m!mine end` - `m!m e`",
+            inline=True
+        )
+        embed.add_field(
+            name="Other commands",
+            value=f"`m!alias mine` - `m!a m`, `m!aliases m`",
+            inline=True
+        )
+        embed.set_author(name = ctx.author.name, icon_url = ctx.author.display_avatar.url)
+        embed.set_footer(text=f"Shard #{ctx.guild.shard_id + 1}")
+        await ctx.send(embed=embed)
+
+    @alias.command(aliases=["c"])
+    async def mine(self, ctx: commands.Context):
+        embed=discord.Embed(
+            title="NotPogMC Command Aliases · m!chop",
+            description="`m!chop` - `m!c`",
+            color=discord.Color(0xfff5d1),
+            timestamp=datetime.datetime.now(datetime.timezone.utc)
+        )
+        embed.add_field(
+            name="Subcommands",
+            value=f"`m!chop overworld` - `m!c o`, `m!c ow`\n\
+                    `m!chop nether` - `m!c n`\n\
+                    `m!chop end` - `m!c e`",
+            inline=True
+        )
+        embed.add_field(
+            name="Other commands",
+            value=f"`m!alias chop` - `m!a c`, `m!aliases c`",
+            inline=True
+        )
+        embed.set_author(name = ctx.author.name, icon_url = ctx.author.display_avatar.url)
+        embed.set_footer(text=f"Shard #{ctx.guild.shard_id + 1}")
+        await ctx.send(embed=embed)
+    
+    @alias.command(aliases=["adv"])
+    async def mine(self, ctx: commands.Context):
+        embed=discord.Embed(
+            title="NotPogMC Command Aliases · m!adventure",
+            description="`m!mine` - `m!adv`",
+            color=discord.Color(0xfff5d1),
+            timestamp=datetime.datetime.now(datetime.timezone.utc)
+        )
+        embed.add_field(
+            name="Subcommands · Dimensional Adventures",
+            value=f"`m!adventure overworld` - `m!adv o`, `m!adv ow`\n\
+                    `m!adventure nether` - `m!adv n`\n\
+                    `m!adventure end` - `m!adv e`",
+            inline=True
+        )
+        embed.add_field(
+            name="Subcommands · Biome Adventures (Overworld)",
+            value=f"`m!adventure desert` - `m!adv d`\n\
+                    `m!adventure jungle` - `m!adv j`\n\
+                    `m!adventure ocean` - `m!adv oc`",
+            inline=True
+        )
+        embed.add_field(
+            name="Subcommands · Biome Adventures (Nether)",
+            value=f"`m!adventure netherwastes` - `m!adv nw`\n\
+                    `m!adventure crimson` - `m!adv cf`, `m!adv crimsonforest`\n\
+                    `m!adventure warped` - `m!adv wf`, `m!adv warpedforest`\n\
+                    `m!adventure soulsand` - `m!adv ss`, `m!adv ssv`, `m!adv soulsandvalley`\n\
+                    `m!adventure basalt` - `m!adv bd`, `m!adv basaltdeltas`",
+            inline=True
+        )
+        embed.add_field(
+            name="Other commands",
+            value=f"`m!alias adventure` - `m!a adv`, `m!aliases adv`",
+            inline=True
+        )
+        embed.set_author(name = ctx.author.name, icon_url = ctx.author.display_avatar.url)
+        embed.set_footer(text=f"Shard #{ctx.guild.shard_id + 1}")
         await ctx.send(embed=embed)
 
 async def setup(bot: commands.Bot):
